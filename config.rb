c48038a898fc70a9fbb1e69c4ca90ecad2f96b76
Aaron Peckham <apeckham@local>
2012-04-17 12:53:20 -0700

    parameterize PORT and fail if any env vars are missing

--- config.rb
+++ config.rb
@@ -1,4 +1,7 @@
 require './s3_driver'
 
+missing = %w[PORT USERNAME PASSWORD ACCESS_KEY_ID SECRET_ACCESS_KEY BUCKET].reject { |name| ENV[name] }
+raise "missing env variables: #{missing}" unless missing.empty?
+
 driver S3Driver
-port 2121
+port ENV['PORT']
